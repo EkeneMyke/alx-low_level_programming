01dae8b8d49d49e9fe5102791b90da2d5f3ab971
EkeneMyke <kennemyke@gmail.com>
2023-07-15 22:58:13 +0100

    Numbers in Base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Description: Print base 10 numbers
+ *Return: Always 0
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n <= 9)
+	{
+		printf("%d\n", n);
+		n++;
+	}
+	return (0);
+}
