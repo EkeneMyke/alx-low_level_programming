c0b528a9c89a9f65526abba4c867c5ee380e87d3
EkeneMyke <kennemyke@gmail.com>
2023-07-19 16:43:59 +0100

    Signs

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -20,15 +20,15 @@ int print_sign(int n)
 		putchar(43);
 		return (1);
 	}
-	else if (n == 0)
-	{
-		putchar(48);
-		return (0);
-	}
-	else
+	else if (n < 0)
 	{
 		putchar(45);
 		return (-1);
+	}
+	else
+	{
+		putchar(48);
+		return (0);
 	}
 	putchar('\n');
 }
