e82ab85d28923d26be354da23b4939c07359fefd
EkeneMyke <kennemyke@gmail.com>
2023-07-19 18:37:42 +0100

    Last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -16,12 +16,12 @@ int print_last_digit(int)
 	l = n % 10;
 	if (l < 0)
 	{
-		_putchar(-l + 48);
+		putchar(-l + 48);
 		return (-l);
 	}
 	else
 	{
-		_putchar(l + 48);
+		putchar(l + 48);
 		return (l);
 	}
 }
