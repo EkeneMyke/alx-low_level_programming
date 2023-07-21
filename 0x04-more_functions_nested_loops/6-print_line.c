7b15400e6806092454a3918a5c3880c0033211e1
EkeneMyke <kennemyke@gmail.com>
2023-07-21 19:53:03 +0100

    Straight Line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -11,13 +11,17 @@
 
 void print_line(int n)
 {
-	for (n = 0; n >= 0; n++)
+	if (n <= 0)
 	{
-		if (n < 0)
-		{
-			_putchar('\n');
-		}
-		_putchar('_');
+		_putchar('\n');
 	}
+	else
+	{
+		int i;
 
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
 }
