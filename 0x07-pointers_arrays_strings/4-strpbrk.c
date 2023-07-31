2e3d7e88d67719aed498f3bbabcc7dbc6e8cd702
EkeneMyke <kennemyke@gmail.com>
2023-07-31 22:56:28 +0100

    strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -2,27 +2,22 @@
 /**
  *_strpbrk - searches a string for any of a set of bytes
  *@s: A pointer to the input string
- *@accept: A pointer to the set of bytes you want to 
+ *@accept: A pointer to the set of bytes you want to
  *search for in the input string
  *Return: Returns 1 or 0
  */
 char *_strpbrk(char *s, char *accept)
 {
-	char *ptr;
+	int index;
 
 	while (*s)
 	{
-		ptr = accept;
-	}
-	while (*accept)
-	{
-		if (*s == *ptr)
+		for (index = 0; accept[index]; index++)
 		{
-			return (1);
+			if (*s == accept[index])
+				return (s);
 		}
-		bytes_ptr++;
+		s++;
 	}
-	str++;
-
-	return (0);
+	return ('\0');
 }
