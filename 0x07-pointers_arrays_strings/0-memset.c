5e4e8a33f7447c715aeeab3436ae2501cdcff2e3
EkeneMyke <kennemyke@gmail.com>
2023-07-31 19:51:39 +0100

    Memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,10 +11,9 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	int i;
 
-	for (i = 0; i < b; i++)
+	for (i = 0; i < n; i++)
 	{
-		s[i] = n;
-		_putchar(s[i]);
+		s[i] = b;
 	}
-	return (0);
+	return (s);
 }
