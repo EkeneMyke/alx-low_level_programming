1e97982e7aff37613547a63ae8468f66fdf1cc0c
EkeneMyke <kennemyke@gmail.com>
2023-08-10 20:45:32 +0100

    Trust no one

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,6 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
 
+void errors(void);
+int _strlen(char *s);
+int is_digit(char *s);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
